dbd14dba8aa5629753bd147784f7de8f042bffa3
Ian Stainer <ian.stainer@valtech.co.uk>
2014-07-10 10:50:50 +0100

    Getting tests working

--- test/IntegrationSpec.scala
+++ test/IntegrationSpec.scala
@@ -16,7 +16,7 @@ class IntegrationSpec extends Specification {
 
     "work from within a browser" in new WithBrowser {
 
-      browser.goTo("http://localhost:" + port)
+      browser.goTo("http://localhost:" + port + "/test")
 
       browser.pageSource must contain("Victoria's Property")
     }
